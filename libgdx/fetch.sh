20e84047262956c1121a73576915663b6a7eed0c
Micah Carrick <micah@quixotix.com>
2012-09-08 09:01:17 -0700

    Added script to download latest libgdx nightly and install files into source tree. Run with ./libgdx/fetch.sh

---
+++ libgdx/fetch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+LIBGDX_ZIP="libgdx-nightly-latest.zip"
+
+message(){
+    echo -e "\E[1;36m"
+    echo $1
+    echo -e "\E[0;37m"
+}
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+message "Downloading $LIBGDX_ZIP ..."
+
+wget http://libgdx.badlogicgames.com/nightlies/$LIBGDX_ZIP
+
+message "Extracting $LIBGDX_ZIP ..."
+
+unzip -o $LIBGDX_ZIP
+rm libgdx-nightly-latest.zip
+
+message "Copying JAR files into source tree ..."
+
+cp -v gdx.jar sources/gdx-sources.jar ../main/libs/
+cp -v gdx-natives.jar gdx-backend-lwjgl.jar gdx-backend-lwjgl-natives.jar sources/gdx-backend-lwjgl-sources.jar ../desktop/libs/
+cp -v gdx-backend-android.jar sources/gdx-backend-android-sources.jar ../android/libs/
+cp -rv armeabi-v7a armeabi ../android/libs/
+
