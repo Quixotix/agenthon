e192e97cace3453eaf786af7d1b2793cec0526be
Micah Carrick <micah@janrain.com>
2013-09-02 13:39:59 -0700

    Updated scripts to fetch libgdx nightlies and pack textures.

---
+++ pack-textures.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -cp libgdx/gdx.jar:libgdx/extensions/gdx-tools/gdx-tools.jar com.badlogic.gdx.tools.imagepacker.TexturePacker2 images android/assets/textures images-packed
